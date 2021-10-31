7cb9acdf07a4a820330dfa1a46269da0770c90a9
mmitusov <maksym.mitusov@gmail.com>
2021-10-31 20:03:45 +0300

    Adding dynamic ports

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.put('/image', (req, res) => {image(req, res, db)});
 app.post('/imageurl', (req, res) => {handleApiCall(req, res)}); //<--- We're fetching to this URL and passing function that runs within imaje.js file 
 
 
-app.listen(3001, () => {
-	console.log('App is running')
+app.listen(process.env.PORT || 3001, () => {
+	console.log(`App is running on port ${process.env.PORT}`)
 })
