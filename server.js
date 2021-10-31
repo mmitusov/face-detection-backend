f9da5e25457e57fe48f152609d14dd047d38c3e8
mmitusov <maksym.mitusov@gmail.com>
2021-10-31 20:10:56 +0300

    Changing root response

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = Express();
 app.use(Express.json())
 app.use(cors())
 
-// app.get('/', (req, res) => {res.json(database.users)})
+app.get('/', (req, res) => {res.json('Homepage is working!')})
 app.post('/register', (req, res) => {handleRegister(req, res, bcrypt, db)});    //!!!<--- Using dependency injection and injecting whatever dependensies this function needs (we can see them all together). So so don't need to import "bcrypt & db" on signin.js page
 app.post('/signin', Signin(bcrypt, db));                 //!!!<--- You can also declare function right away and then (req, res) will be called automaticly after (bcrypt, db)
 app.get('/profile/:id', (req, res) => {profileId(req, res, db)});
